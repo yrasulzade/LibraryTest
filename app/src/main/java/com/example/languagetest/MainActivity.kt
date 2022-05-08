86db0a5ce3e0b07f448a536eae91231781e39ed5
yunisrasulzade <yunisrasulzada99@gmail.com>
2022-05-08 11:16:26 +0400

    Initial commit

---
+++ app/src/main/java/com/example/languagetest/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.languagetest
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.languagetest.databinding.ActivityMainBinding
+import com.example.whelp.WhelpLauncherActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, WhelpLauncherActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ whelp/src/main/java/com/example/whelp/WhelpLauncherActivity.kt
@@ -0,0 +1,11 @@
+package com.example.whelp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WhelpLauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_whelp_launcher)
+    }
+}
