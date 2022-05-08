42803f480197da31da7eced58ae05a5a85d26a80
yunisrasulzade <yunisrasulzada99@gmail.com>
2022-05-08 12:09:21 +0400

    1.1 version

--- whelp/src/main/java/com/example/whelp/WhelpLauncherActivity.kt
+++ whelp/src/main/java/com/example/whelp/WhelpLauncherActivity.kt
@@ -2,10 +2,13 @@ package com.example.whelp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class WhelpLauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_whelp_launcher)
+
+        Toast.makeText(this, "This is 1.1 version", Toast.LENGTH_LONG).show()
     }
 }
